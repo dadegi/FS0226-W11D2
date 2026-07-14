26802db05c0da7f7a1b35f477742c7b4257e6be3
Naviganet <dadegi@gmail.com>
2026-07-14 14:36:33 +0200

    Codici lezione W11D2

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,3 +1,70 @@
-const Cart = () => {};
+import { Col, Row, Button } from 'react-bootstrap';
+import { FaTrash } from 'react-icons/fa';
+import { useSelector, useDispatch } from 'react-redux';
+import { removeFromCartAction } from '../redux/actions/actions';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router';
+
+const Cart = () => {
+	const myCart = useSelector((state) => {
+		return state.cart.content;
+	});
+
+	const availableBooks = useSelector((state) => {
+		return state.books.available;
+	});
+	const userName = useSelector((state) => {
+		return state.user.name;
+	});
+
+	const dispatch = useDispatch();
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		if (!userName) {
+			navigate('/');
+		}
+	});
+
+	return (
+		<Row>
+			<Col sm={12}>
+				<ul style={{ listStyle: 'none' }}>
+					{myCart.map((book, i) => (
+						<li key={i} className='my-4'>
+							<Button
+								variant='danger'
+								onClick={() => {
+									dispatch(removeFromCartAction(book.id));
+								}}
+							>
+								<FaTrash />
+							</Button>
+							<img
+								className='book-cover-small'
+								src={book.imageUrl}
+							/>
+							{book.title}
+						</li>
+					))}
+				</ul>
+			</Col>
+			<Row>
+				<Col sm={12} className='fw-bold mb-3'>
+					TOTALE:{' '}
+					{myCart.reduce(
+						(acc, value) => acc + parseFloat(value.price),
+						0,
+					)}
+				</Col>
+			</Row>
+			<Row>
+				<Col sm={12} className='fw-bold mb-3'>
+					<p>Libri disponibili: {availableBooks.length}</p>
+				</Col>
+			</Row>
+		</Row>
+	);
+};
 
 export default Cart;
--- src/components/CartIndicator.jsx
+++ src/components/CartIndicator.jsx
@@ -1,6 +1,62 @@
-
+import { Form, Button } from 'react-bootstrap';
+import { useNavigate } from 'react-router';
+import { FaShoppingCart } from 'react-icons/fa';
+import { useSelector, useDispatch } from 'react-redux';
+import { useState } from 'react';
+import { setUsernameAction } from '../redux/actions/actions';
 
 const CartIndicator = () => {
+	const navigate = useNavigate();
+
+	const [value, setValue] = useState('');
+	const dispatch = useDispatch();
+
+	const cartLength = useSelector((state) => {
+		return state.cart.content.length;
+	});
+	const name = useSelector((state) => {
+		return state.user.name;
+	});
+
+	return (
+		<div className='d-flex justify-content-end my-4'>
+			{name ? (
+				<span className='d-flex align-items-center'>
+					<span className='me-2'>Benvenuto, {name}</span>
+					<Button
+						onClick={() => navigate('/cart')}
+						className='d-flex align-items-center'
+					>
+						<FaShoppingCart />
+						<span className='ms-2'>{cartLength}</span>
+					</Button>
+				</span>
+			) : (
+				<Form
+					className='d-flex'
+					onSubmit={(e) => {
+						e.preventDefault();
+						if (!value.trim()) {
+							return;
+						}
+						dispatch(setUsernameAction(value));
+					}}
+				>
+					<Form.Control
+						type='text'
+						value={value}
+						onChange={(e) => {
+							setValue(e.target.value);
+						}}
+						required
+					/>
+					<Button variant='primary' type='submit'>
+						VAI
+					</Button>
+				</Form>
+			)}
+		</div>
+	);
 };
 
 export default CartIndicator;
